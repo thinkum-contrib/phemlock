542f068c6c9da23479c20cf620858bfca02127e8
Gilbert Baumann <gbaumann@common-lisp.net>
2004-07-09 13:39:10 +0000

    import

--- src/tty-stuff.lisp
+++
@@ -1,8 +0,0 @@
-;;; Terminal hunks.
-;;; 
-(defstruct (tty-hunk #|(:print-function %print-device-hunk)|#
-		     (:include device-hunk))
-  text-position		; Bottom Y position of text in hunk.
-  text-height)		; Number of lines of text.
-
-
