eca3ac473c3d877460eda8de20be0fe5845a10d2
Gilbert Baumann <gbaumann@common-lisp.net>
2004-07-09 13:39:14 +0000

    *** empty log message ***

---
+++ src/user/bindings-gb.lisp
@@ -0,0 +1,13 @@
+(in-package :hemlock)
+
+(bind-key "Scroll Window Down" #k"pagedown")
+(bind-key "Scroll Window Up"   #k"pageup")
+(bind-key "Undo"               #k"control-\/")
+(bind-key "Help"               #k"control-h")
+(bind-key "Dabbrev Expand"     #k"meta-/")
+(bind-key "just one space"     #k"meta-space")
+
+  
+
+
+
